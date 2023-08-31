c83aa56a3ef1a8572190022b8a588d34120d9830
an2629 <anjalichouksey881@gmail.com>
2023-08-31 19:23:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function test()
+{
+    //Retrieving the  data
+    var id= document.getElementById("username").value;
+    var el= document.getElementById("email").value;
+    var ph =document.getElementById("phone").value;
+
+
+    //Storing the data
+    var n= localStorage.setItem("id",id);
+    var e= localStorage.setItem("el",el);
+    var p= localStorage.setItem("ph",ph);
+
+    //Retrieving the store data
+
+    
+}
+
+
+
+
